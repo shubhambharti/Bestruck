5da8d7c5ecc6bd1101449766107ea791bfd5ff32
Shubham Bharti <shubhambharti711@gmail.com>
2025-10-03 19:30:18 +0530

    top

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,12 +1,46 @@
-import { useEffect } from "react";
+// src/components/ScrollToTop.jsx
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { FaArrowUp } from 'react-icons/fa'; // Import the icon
+import { FaPersonArrowUpFromLine } from "react-icons/fa6";
 
 export default function ScrollToTop() {
     const { pathname } = useLocation();
+    const [isButtonVisible, setIsButtonVisible] = useState(false);
 
     useEffect(() => {
         window.scrollTo(0, 0);
+        setIsButtonVisible(false);
     }, [pathname]);
 
-    return null;
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.scrollY > 300) {
+                setIsButtonVisible(true);
+            } else {
+                setIsButtonVisible(false);
+            }
+        };
+
+        window.addEventListener('scroll', toggleVisibility);
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+
+    const handleScrollToTopClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+    return (
+        <button
+            className={`scroll-to-top btn ${isButtonVisible ? 'show' : ''}`}
+            onClick={handleScrollToTopClick}
+            aria-label="Scroll to top"
+        >
+            <FaPersonArrowUpFromLine />
+        </button>
+    );
 }
